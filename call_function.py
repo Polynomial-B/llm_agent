e52c08061a262b514f52b25c478e1f21ac152b92
Polynomial-B <mphlamb@gmail.com>
2026-03-13 18:31:57 +0100

    added call_function and result validation

--- call_function.py
+++ call_function.py
@@ -1,5 +1,9 @@
 from google.genai import types
 from functions.function_schemas import *
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 available_functions = types.Tool(
     function_declarations=[
@@ -9,4 +13,47 @@ available_functions = types.Tool(
         schema_run_python_file
         ],
 )
+
+def call_function(function_call, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
+    
+    function_map = {
+        "get_file_content": get_file_content,
+        "get_files_info": get_files_info,
+        "write_file": write_file,
+        "run_python_file": run_python_file,
+    }
+    
+    fn_name = function_call.name or ""
+    
+    if fn_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call.name,
+                    response={"error": f"Unknown function: {function_call.name}"},
+                )
+            ],
+        )
+    
+    args = dict(function_call.args) if function_call.args else {}
+
+    args["working_directory"] = "./calculator"
+
+    result = function_map[fn_name](**args)
+
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=fn_name,
+                response={"result": result},
+            )
+        ],
+)
+
 
--- main.py
+++ main.py
@@ -3,45 +3,57 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if api_key is None:
-    raise RuntimeError("Error getting API Key")
-
-# ---------- CLIENT ----------
-client = genai.Client(api_key=api_key)
-
-# --------- ARGPARSE ---------
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
-
-# ------- MESSAGES LIST ------
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
-# --------- RESPONSE ---------
-response = client.models.generate_content(
-    model='gemini-2.5-flash', 
-    contents=messages,
-    config=types.GenerateContentConfig(tools=[available_functions],
-        system_instruction=system_prompt, max_output_tokens=200), # ! MAX TOKEN LIMIT ADDED
-)
-if args.verbose:
-    print("User prompt: " + str(args.user_prompt))
-    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
-
-if response.function_calls:
-    for f in response.function_calls:
-        print(f"Calling function: {f.name}({f.args})")
-else:
-    print(response.text)
+from call_function import available_functions, call_function
 
 def main():
-    pass
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("Error getting API Key")
+
+    # ---------- CLIENT ----------
+    client = genai.Client(api_key=api_key)
+
+    # --------- ARGPARSE ---------
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    # ------- MESSAGES LIST ------
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    # --------- RESPONSE ---------
+    response_list = []
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions],
+            system_instruction=system_prompt, max_output_tokens=200), # ! MAX TOKEN LIMIT ADDED
+    )
+    if args.verbose:
+        print("User prompt: " + str(args.user_prompt))
+        print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+        print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+
+    if response.function_calls:
+        result_call_function = call_function(response.function_calls[0], verbose=args.verbose)
+        if not result_call_function.parts[0]:
+            raise Exception("Error: an error occurred")
+        if result_call_function.parts[0].function_response == None:
+            raise Exception("Error: an error occurred")
+        if result_call_function.parts[0].function_response.response == None:
+            raise Exception("Error: an error occurred")
+        response_list.append(result_call_function.parts[0])
+        
+        if args.verbose:
+            print(f"-> {result_call_function.parts[0].function_response.response}")
+        
+    else:
+        print(response.text)
+
 
 if __name__ == "__main__":
     main()
